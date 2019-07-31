ffc4a66eaeedbb60adad82619e06cba282fa417e
kiarmor <kiarmor2017@gmail.com>
2019-07-31 22:04:49 +0300

    first step

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
