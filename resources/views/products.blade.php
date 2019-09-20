cb33f34ba379f8be1623830a27431daa6fc84ee1
kiarmor <kiarmor2017@gmail.com>
2019-09-20 20:40:41 +0300

    product list

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,4 +5,23 @@
 
 @section('content')
     <p>This is our products.</p>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+            <th>ID</th>
+            <th>Product</th>
+            <th>Category</th>
+            </thead>
+            <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <td><a href="http://localhost:8000/posts/{{$product['id']}}">{{$product['id']}}</a></td>
+                    <td>{{$product['name']}}</td>
+                    <td>{{$product['category_id']}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
