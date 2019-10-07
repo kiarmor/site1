1587b18669b3c2f4182ac17294db2d12823e97b3
Kiarmor <kiarmor2017@gmail.com>
2019-10-07 23:58:27 +0300

    css and js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,29 +2,65 @@
 <html lang="en">
 <head>
         <title>@yield('title')</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!-- Подключаем Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
+   {{-- <!-- Подключаем Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />--}}
+        @yield('styles')
 
 </head>
 <body>
 
-
-        <a href="/">Home page</a>
-        <a href="/news">News</a>
-        <a href="/about">About Us</a>
-        <a href="contact">Contact</a>
-        <a href="/posts">Posts</a>
-        <a href="/products">Products</a>
-        <a href="/home">Login</a>
-        @if(isset($auth))
-                @if($auth->role == 1)
-                        <a href="/admin">Admin panel</a>
-                @endif
-        @endif
-
-
-        <form method="GET" action="/search">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="/news">News</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="/about">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="/posts">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="/products">Products</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="/home">Login</a>
+                        </li>
+                        @if(isset($auth))
+                                @if($auth->role == 1)
+                                        <li class="nav-item">
+                                                <a class="nav-link" href="/admin">Admin panel</a>
+                                        </li>
+                                @endif
+                        @endif
+                        {{--<li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Dropdown
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>--}}
+                </ul>
+                <form class="form-inline my-2 my-lg-0"  method="GET" action="/search">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+        </div>
+</nav>
+       {{-- <form method="GET" action="/search">
                 <div class="field">
 
                         <label class="label" for="search">Search</label>
@@ -39,15 +75,17 @@
                                 <button type="submit" class="button">Search product</button>
                         </div>
                 </div>
-        </form>
+        </form>--}}
 
 
         @yield('content')
 
 
-        <a href="/">How to find us.</a>
 
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        @yield('scripts')
 </body>
 </html>
 
