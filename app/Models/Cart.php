f01d2466594d9fe680641c79ee50c4c3732b2e44
kiarmor <kiarmor2017@gmail.com>
2019-10-09 22:58:10 +0300

    temp commit

---
+++ app/Models/Cart.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+class Cart
+{
+   public $items = null;
+   public $totalQty = 0;
+   public $totalPrice = 0;
+
+    public function __construct($oldCart)
+    {
+         if ($oldCart){
+             $this->items = $oldCart['items'];
+             $this->totalQty = $oldCart['totalQty'];
+             $this->totalPrice = $oldCart['totalPrice'];
+         }
+    }
+
+    public function add($item, $productId)
+    {
+        $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
+        if ($this->items){
+            if (array_key_exists($productId, $this->items)){
+                $storedItem = $this->items[$productId];
+            }
+        }
+        $storedItem['qty']++;
+        $storedItem['price'] = $item->price * $storedItem['qty'];
+        $this->items[$productId] = $storedItem;
+        $this->totalQty++;
+        $this->totalPrice += $item->price;
+    }
+}
---
+++ resources/views/temp.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anonimus
+ * Date: 08.10.2019
+ * Time: 22:00
+ */
